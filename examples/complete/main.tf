0466cd2b419d63aacf38fbe79d0d46447d758657
ankur64 <ankur@squareops.com>
2023-07-21 19:31:06 +0530

    Updateeck to use karpenter module

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -10,17 +10,19 @@ locals {
 }
 
 module "eck" {
-  source       = "https://github.com/sq-ia/terraform-kubernetes-elastic.git"
+  source       = "../.."  #"https://github.com/sq-ia/terraform-kubernetes-elastic.git"
   cluster_name = "dev-cluster"
   eck_config = {
     hostname          = "eck.squareops.in"
     eck_values        = file("./helm/eck.yaml")
     karpenter_enabled = true
+    ipv6_enabled      = false
     karpenter_config = {
+      provisioner_name       = "eck-karpenter-provisioner"
+      provisioner_values     = file("./helm/karpenter.yaml")
       private_subnet_name    = "private-subnet-name"
       instance_capacity_type = ["spot"]
       excluded_instance_type = ["nano", "micro", "small"]
-      karpenter_eck_values   = file("./helm/karpenter.yaml")
     }
     master_node_sc       = "gp2"
     data_hot_node_sc     = "gp2"
